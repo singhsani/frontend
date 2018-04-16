dd7794750e69fbdf32dafe5971207e8ab70f2b4f
Kaushik Andani <kaushik.andani@nascentinfo.com>
2018-04-16 18:04:18 +0530

    Add Language Translation

---
+++ src/app/shared/modules/translate/translate.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from './translate.service';
+
+@Pipe({
+	name: 'translate',
+	pure: false
+})
+export class TranslatePipe implements PipeTransform {
+
+	constructor(
+		private translateService: TranslateService
+	) { }
+
+	transform(value: string, args?: string): string {
+
+		if(args){
+			return this.translateService.translate(value, args);
+		} else {
+
+		}
+	}
+
+}
