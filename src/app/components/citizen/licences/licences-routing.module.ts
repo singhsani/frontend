b325cc3a9a6dbd4e723df4f96b2c7a84e04da3c5
Kaushik Andani <kaushik.andani@nascentinfo.com>
2018-04-30 11:28:57 +0530

    Add Lincence module with Shop and Mutton Fish Basic Components

--- src/app/components/citizen/licences/licences-routing.module.ts
+++ src/app/components/citizen/licences/licences-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../../../core/guard/auth.guard';
+import { ShopEstablishLicenceComponent } from './shop-establish-licence/shop-establish-licence.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', redirectTo: 'shopLicense', pathMatch: 'full' },
+	{ path: 'shopLicense/:id', component: ShopEstablishLicenceComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
 export class LicencesRoutingModule { }
---
+++ src/app/components/citizen/licences/shop-establish-licence/shop-establish-licence.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-establish-licence',
+  templateUrl: './shop-establish-licence.component.html',
+  styleUrls: ['./shop-establish-licence.component.scss']
+})
+export class ShopEstablishLicenceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
