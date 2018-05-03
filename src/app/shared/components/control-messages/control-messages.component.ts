582a73ef028f2de6cde11cb9578fcb03572083a7
bhumika.barad <bhumika.barad@nascentinfo.com>
2018-05-03 14:42:54 +0530

    fileupload-validationService-submitButton-addressComponent

--- src/app/shared/components/control-messages/control-messages.component.ts
+++ src/app/shared/components/control-messages/control-messages.component.ts
@@ -21,6 +21,9 @@ export class ControlMessagesComponent {
 			if (this.control.errors.hasOwnProperty(propertyName) && this.control.touched) {
 				return ValidationService.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
 			}
+			if(this.control.valueChanges){
+				return ValidationService.showValidatorErrorMessage(propertyName, this.control.errors[propertyName])
+			}
 		}
 
 		return null;
