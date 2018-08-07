e2707c934f481e247b585ec326320d4fb4ef6dba
bhumika.barad <bhumika.barad@nascentinfo.com>
2018-08-07 11:07:49 +0530

    Remove focus event

--- src/app/shared/directives/alpha-numeric.directive.spec.ts
+++ src/app/shared/directives/alpha-numeric.directive.spec.ts
@@ -1,8 +1,5 @@
 import { AlphaNumericDirective } from './alpha-numeric.directive';
 
 describe('AlphaNumericDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AlphaNumericDirective();
-    expect(directive).toBeTruthy();
-  });
+
 });
