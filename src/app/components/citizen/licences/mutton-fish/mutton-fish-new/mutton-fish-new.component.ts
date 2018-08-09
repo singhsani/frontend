2080766d49ea45c8b96cb38961541dfd03cc1b18
bhumika.barad <bhumika.barad@nascentinfo.com>
2018-08-09 16:18:17 +0530

    MF new license

--- src/app/components/citizen/licences/mutton-fish/mutton-fish-new/mutton-fish-new.component.ts
+++ src/app/components/citizen/licences/mutton-fish/mutton-fish-new/mutton-fish-new.component.ts
@@ -1,95 +1,400 @@
-import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
-import { MatHorizontalStepper, MatStep, MatStepLabel } from '@angular/material';
 import { ManageRoutes } from './../../../../../config/routes-conf';
+import { CommonService } from '../../../../../shared/services/common.service';
 
 import { ValidationService } from '../../../../../shared/services/validation.service';
 import { FormsActionsService } from '../../../../../core/services/citizen/data-services/forms-actions.service';
 import * as _ from 'lodash';
+import * as moment from 'moment';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
 	selector: 'app-mutton-fish-new',
 	templateUrl: './mutton-fish-new.component.html',
 	styleUrls: ['./mutton-fish-new.component.scss']
 })
+
 export class MuttonFishNewComponent implements OnInit {
 
-
-	@ViewChild(MatHorizontalStepper) stepper: MatHorizontalStepper;
-	@ViewChild(MatStepLabel) steplable: MatStepLabel;
+	@ViewChild('permanantAddressEstablishment') permanantAddressEstablishment: any;
 
 	muttonFishNewForm: FormGroup;
 	translateKey: string = 'muttonFishNewScreen';
 
-	appId: number;
+	formId: number;
 	apiCode: string;
+	tabIndex: number = 0;
 
-	// Step Titles
-	stepLable1: string = "Applicant Basic Details";
+	//File and image upload
+	uploadModel: any = {};
+	private showButtons: boolean = false;
 
+	//Lookups Array
+	MF_LICENSE_TYPE: Array<any> = [];
+	MF_RELATIONSHIP_OF_APPLICANT: Array<any> = [];
+	MF_STATUS_OF_BUSINESS: Array<any> = [];
+	PERSON_TYPE: Array<any> = [];
+	FIRM_ZONE: Array<any> = [];
+	WARD: Array<any> = [];
+	BLOCK: Array<any> = [];
+	LOOKUP: any;
+	businessCategory: Array<any> = [];
+	businessSubCategory: Array<any> = [];
+
+	// required attachment array
+	private uploadFileArray: Array<any> =
+		[
+			{ labelName: 'Photo of License Holder', fieldIdentifier: '1', category: 'common' },
+			{ labelName: 'One Copy of each site plan and key plan', fieldIdentifier: '2', category: 'common' },
+			{ labelName: 'Aadhar Card Scan Copy', fieldIdentifier: '3', category: "common" },
+			{ labelName: 'Pan Card Copy of Owner / Propwriter', fieldIdentifier: '4', category: "common" },
+			// { labelName: 'Constitution copy of Firm', fieldIdentifier: '5', category: "common" },
+			{ labelName: 'Proof of Ownership / tenancy / Legal Occupancy', fieldIdentifier: '6', category: "common" },
+			{ labelName: 'Copy of Lease Deed If not Executed, Copy of Auction Letter, Possession Letter', fieldIdentifier: '7', category: "common" },
+			// { labelName: 'Copy of Term & conditions for allotment of Premises by the  Land owning Agency ', fieldIdentifier: '8', category: "common" },
+			{ labelName: 'Additional Document if Any', fieldIdentifier: '9', category: "common" },
+			{ labelName: 'Property Tax Bill paid Receipt', fieldIdentifier: '10', category: "common" },
+			{ labelName: 'Shop & Establishment Certificate', fieldIdentifier: '11', category: "common" },
+
+			{ labelName: 'Occupation Certificate', fieldIdentifier: '12', category: "rent" },
+			{ labelName: 'Rent Agreement', fieldIdentifier: '13', category: "rent" }
+		];
+
+	get uploadFileArrayData() {
+		let data = this.uploadFileArray;
+		if (this.muttonFishNewForm.get('statusOfBusinessId').value.code != 'RENT') {
+			data = this.uploadFileArray.filter((obj) => obj.category != 'rent');
+		}
+		return data
+	}
+
+    /**
+     * @param fb - Declare FormBuilder property.
+     * @param validationError - Declare validation service property
+     * @param formService - Declare form service property 
+     * @param uploadFileService - Declare upload file service property.
+     * @param commonService - Declare sweet alert.
+	 * @param toastrService - Show massage with timer.
+     */
 	constructor(
 		private fb: FormBuilder,
 		private validationService: ValidationService,
 		private router: Router,
 		private route: ActivatedRoute,
-		private formService: FormsActionsService
+		private formService: FormsActionsService,
+		private commonService: CommonService,
+		private toastrService: ToastrService
 	) { }
 
+	/**
+	 * This method call initially required methods.
+	 */
 	ngOnInit() {
-
 		this.route.paramMap.subscribe(param => {
-			this.appId = Number(param.get('id'));
+			this.formId = Number(param.get('id'));
 			this.apiCode = param.get('apiCode');
 			this.formService.apiType = ManageRoutes.getApiTypeFromApiCode(this.apiCode);
 		});
 
-		this.getMuttonFishNewData();
-		this.getLookupData();
-		this.muttonFishNewFormControls();
+		if (!this.formId) {
+			this.router.navigate([ManageRoutes.getFullRoute('CITIZENDASHBOARD')]);
+		}
+		else {
+			this.getMuttonFishLicNewData();
+			this.getLookupData();
+			this.muttonFishNewFormControls();
+		}
 	}
 
-	getMuttonFishNewData() {
-		this.formService.getFormData(this.appId).subscribe(res => {
-			this.muttonFishNewForm.patchValue(res);
+	/**
+	 * Method is used to get form data
+	 */
+	getMuttonFishLicNewData() {
+		this.formService.getFormData(this.formId).subscribe(res => {
+			try {
+				this.muttonFishNewForm.patchValue(res);
+				this.showButtons = true;
+				res.relationshipList.forEach(app => {
+					(<FormArray>this.muttonFishNewForm.get('relationshipList')).push(this.createArray(app));
+				});
+			} catch (error) {
+				console.log(error.message)
+			}
 		});
 	}
 
 	/**
-	 * Method is used to handle error/validation on submit
-	 * @param count - count of invalid control.
-	 */
-	handleErrorsOnSubmit(count) {
-		let step1 = 6;
-
-		if (count <= step1) {
-			this.stepper.selectedIndex = 0;
-			return false;
-		}
-
-	}
-
-	/**
-	 * This method is use for get lookup data
-	 */
+	* Method is used to get lookup data
+	*/
 	getLookupData() {
 		this.formService.getDataFromLookups().subscribe(res => {
-
-		});
-	}
-
-	muttonFishNewFormControls() {
-		this.muttonFishNewForm = this.fb.group({
-			apiType: ManageRoutes.getApiTypeFromApiCode(this.apiCode)
+			this.LOOKUP = res;
+			this.MF_LICENSE_TYPE = res.MF_LICENSE_TYPE;
+			this.MF_RELATIONSHIP_OF_APPLICANT = res.MF_RELATIONSHIP_OF_APPLICANT;
+			this.MF_STATUS_OF_BUSINESS = res.MF_STATUS_OF_BUSINESS;
+			this.PERSON_TYPE = res.PERSON_TYPE;
+			this.FIRM_ZONE = res.FIRM_ZONE;
 		});
 	}
 
 	/**
-	 * Method is used to reset form its a output event from action bar.
+	* Method is used for get WARD as per zone selection
+	*/
+	onChangeZone(event) {
+		this.WARD = [];
+		if (event && this.LOOKUP.hasOwnProperty(event)) {
+			this.WARD = this.LOOKUP[event];
+		}
+	}
+
+	/**
+	* Method is used for get block as per zone selection
+	*/
+	onChangeWard(event) {
+		this.BLOCK = [];
+		if (event && this.LOOKUP.hasOwnProperty(event)) {
+			this.BLOCK = this.LOOKUP[event];
+		}
+	}
+
+	/**
+	* Method is used to set form controls
+	* 'Guj' control is consider as a Gujarati fields
+	*/
+	muttonFishNewFormControls() {
+		this.muttonFishNewForm = this.fb.group({
+			apiType: ManageRoutes.getApiTypeFromApiCode(this.apiCode),
+			serviceCode: 'MF-LIC',
+			/* Step 1 controls start */
+			licenseType: this.fb.group({
+				code: [null]
+			}),
+			personType: this.fb.group({
+				code: [null]
+			}),
+			holderFirstName: [null, [Validators.required, Validators.maxLength(30)]],
+			holderMiddleName: [null, [Validators.required, Validators.maxLength(30)]],
+			holderLastName: [null, [Validators.required, Validators.maxLength(30)]],
+			holderFirstNameGuj: [null, [Validators.required, Validators.maxLength(90)]],
+			holderMiddleNameGuj: [null, [Validators.required, Validators.maxLength(90)]],
+			holderLastNameGuj: [null, [Validators.required, Validators.maxLength(90)]],
+
+			permanantAddress: this.fb.group(this.permanantAddressEstablishment.addressControls()),
+			temporaryAddress: this.fb.group(this.permanantAddressEstablishment.addressControls()),
+
+			holderTelephoneNo: [null, [Validators.maxLength(12)]],
+			holderMobileNo: [null, [Validators.required, Validators.maxLength(11)]],
+			holderFaxNo: [null, [Validators.maxLength(12)]],
+			holderAadharNo: [null, [Validators.required, Validators.maxLength(12)]],
+			holderPanNo: [null, [Validators.required, Validators.maxLength(10)]],
+			/* Step 1 controls end */
+
+			/* Step 2 controls start */
+			zoneNo: this.fb.group({ code: [null, Validators.required] }),
+			wardNo: this.fb.group({ code: [null, Validators.required] }),
+			blockNo: this.fb.group({ code: [null, Validators.required] }),
+			businessAddress: this.fb.group(this.permanantAddressEstablishment.addressControls()),
+			extraDetailsOfBusiness: [null, [Validators.maxLength(500)]],
+			relationshipId: this.fb.group({
+				code: [null, Validators.required]
+			}),
+			statusOfBusinessId: this.fb.group({
+				code: [null, Validators.required]
+			}),
+			/* Step 2 controls end */
+
+			/* Step 3 controls start */
+			relationshipList: this.fb.array([]),
+			/* Step 3 controls end */
+
+			applicationDate: [],
+			licenseIssueDate: [null],
+			licenseRenewalDate: [null],
+			loinumber: [null],
+
+			/* Step 4 controls start*/
+			attachments: []
+			/* Step 4 controls end */
+		});
+	}
+
+	/**
+	 * Method is used to return array
+	 * @param data : person data array 
+	 * @param persontype : person array type 
 	 */
-	stepReset() {
-		this.stepper.reset();
+	createArray(data?: any, persontype?: string) {
+
+		return this.fb.group({
+			serviceFormId: this.formId,
+			id: data.id ? data.id : null,
+			name: [data.name ? data.name : null, [Validators.required, Validators.maxLength(100)]],
+			address: [data.address ? data.address : null, [Validators.required, Validators.maxLength(200)]],
+			mobileNo: [data.mobileNo ? data.mobileNo : null, [Validators.maxLength(11)]],
+			personType: "MF_PERSON"
+		})
+
+	}
+
+	/**
+	 * Method is used to add array in form
+	 * @param persontype : person array type
+	 */
+	addItem(persontype: string) {
+		let returnArray: any;
+		switch (persontype) {
+			case 'MF_PERSON':
+				returnArray = this.muttonFishNewForm.get('relationshipList') as FormArray;
+				break;
+		}
+		return returnArray;
+	}
+
+	/**
+	 * Method is used when user click for add person
+	 * @param persontype : person array type
+	 */
+	addMorePerson(persontype: string) {
+
+		let isEditAnotherRow = this.isTableInEditMode(persontype);
+		if (!isEditAnotherRow) {
+			if (persontype === "MF_PERSON" && this.addItem(persontype).controls.length >= 5) {
+				this.toastrService.warning("Person not allowed more than 5");
+				return false;
+			}
+
+			this.addItem(persontype).push(this.createArray({
+				personType: persontype
+			}));
+			// this.muttonFishNewForm.get('relationshipList').setValidators([Validators.required]);
+			let newlyadded = <any>this.addItem(persontype).controls;
+			if (newlyadded.length) {
+				(newlyadded[newlyadded.length - 1]).isEditMode = true;
+			}
+		}
+		else {
+			this.commonService.openAlert("Warning", "You can add new recode after save existing recode.", "warning");
+		}
+	}
+
+	/**
+     * Method is used to set data value to upload method.
+     * @param indentifier - file identifier
+     * @param labelName - file label name.
+     * @param formPart - file form part
+     * @param variableName - file variable name.
+     */
+	setDataValue(indentifier: number, labelName: string, formPart: string, variableName: string) {
+		this.uploadModel = {
+			fieldIdentifier: indentifier.toString(),
+			labelName: labelName,
+			formPart: formPart,
+			variableName: variableName,
+			serviceFormId: this.formId,
+		}
+		return this.uploadModel;
+	}
+
+	/**
+	*  Method is used check table is in edit mode
+	*/
+	isTableInEditMode(persontype: string) {
+		return this.addItem(persontype).controls.find((obj: any) => obj.isEditMode === true);
+	}
+
+	/**
+	*  Method is used edit editable data view.
+	* @param row: table row id
+	*/
+	editRecord(row: any) {
+		row.isEditMode = true;
+		row.deepCopyInEditMode = Object.assign({}, row.value)
+	}
+
+	/**
+	* Method is used when user click for remove person
+	*/
+	deleteRecord(persontype: string, index: any) {
+		this.commonService.confirmAlert('Are you sure?', "", 'info', '', performDelete => {
+			this.addItem(persontype).controls.splice(index, 1);
+			this.commonService.successAlert('Removed!', '', 'success');
+		});
+	}
+
+	/**
+	*  Method is used save editable dataview.
+	* @param row: table row id
+	*/
+	saveRecord(row: any) {
+		if (row.valid) {
+			row.isEditMode = false;
+		}
+	}
+
+	/**
+	*  Method is used cancel editable dataview.
+	* @param row: table row id
+	*/
+	cancelRecord(row: any) {
+		if (row.deepCopyInEditMode) {
+			row.patchValue(row.deepCopyInEditMode);
+		}
+		row.isEditMode = false;
+	}
+
+    /**
+     * This method required for final form submition.
+     * @param flag - flag of invalid control.
+     */
+	handleErrorsOnSubmit(flag) {
+
+		let step0 = 15;
+		let step1 = 27;
+		let step2 = 35;
+		let step3 = 41;
+		let step4 = 48;
+
+		if (flag != null) {
+			//Check validation for step by step
+			let count = flag;
+
+			if (count <= step0) {
+				this.tabIndex = 0;
+				return false;
+			} else if (count <= step1) {
+				this.tabIndex = 1;
+				return false;
+			} else if (count <= step2) {
+				this.tabIndex = 2;
+				return false;
+			} else if (count <= step3) {
+				this.tabIndex = 3;
+				return false;
+			} else if (count <= step4) {
+				this.tabIndex = 4;
+				return false;
+			}
+			// else if (count == 67) {
+			// 	this.checkReligion();
+			// 	return false;
+			// }
+			else {
+				console.log("else condition");
+			}
+
+		}
+	}
+
+	/**
+	 * This method use to get output event of tab change
+	 * @param evt - Tab index
+	 */
+	onTabChange(evt) {
+		this.tabIndex = evt;
 	}
 
 
 }
+
