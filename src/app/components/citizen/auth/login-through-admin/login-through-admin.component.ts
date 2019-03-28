469b25dca25ae7c45823a32b0bd9a4a412f6ce7d
aniket123 <aniket.avhad@nascentinfo.com>
2019-03-28 18:22:50 +0530

    login thorugh admin changes

--- src/app/components/citizen/auth/login-through-admin/login-through-admin.component.ts
+++ src/app/components/citizen/auth/login-through-admin/login-through-admin.component.ts
@@ -4,6 +4,7 @@ import { SessionStorageService } from 'angular-web-storage';
 import { FormsActionsService } from '../../../../core/services/citizen/data-services/forms-actions.service';
 import { ManageRoutes } from '../../../../config/routes-conf';
 import { CommonService } from '../../../../shared/services/common.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
 	selector: 'app-login-through-admin',
@@ -20,6 +21,7 @@ export class LoginThroughAdminComponent implements OnInit {
 		public router: Router,
 		public route: ActivatedRoute,
 		public commonService: CommonService,
+		private toastr: ToastrService,
 		private formService: FormsActionsService,
 	) { }
 
@@ -31,7 +33,7 @@ export class LoginThroughAdminComponent implements OnInit {
 				this.apiCode = param['apiCode'];
 				if (this.accessToken && this.apiCode) {
 					this.saveToken(this.apiCode);
-					
+
 				} else {
 					this.commonService.openAlert('Error', 'Access Token Not Available', 'warning');
 				}
@@ -47,17 +49,65 @@ export class LoginThroughAdminComponent implements OnInit {
 	saveToken(apiCode) {
 		this.session.set('access_token', { 'token': this.accessToken, now: +new Date }, 999999, 's');
 		this.session.set('fromAdmin', 'fromAdmin', 999999, 's');
-		
+
 		switch (apiCode) {
-			case 'PEC_REG':
+			case 'HEL-BCR':
+			case 'HEL-DCR':
+			case 'HEL-NRCBR':
+			case 'HEL-NRCDR':
+			case 'HEL-DUPBR':
+			case 'HEL-DUPDR':
+			case 'HEL-DUPMR':
+			case 'SHOP-REN':
+			case 'SHOP-TRAF':
+			case 'SHOP-CAN':
+			case 'SHOP-DUP':
+			case 'MF-REN':
+			case 'MF-TRA':
+			case 'MF-DUP':
+			case 'MF-CAN':
+			case 'APL-REN':
+			case 'APL-TRA':
+			case 'APL-CAN':
+			case 'APL-DUP':
+			case 'FL-REN':
+			case 'FL-MODIFY':
+			case 'FL-DUP':
+			case 'FS-FINAL':
+			case 'FS-REVISED':
+			case 'FS-REN':
+			case 'FS-FINAL-HOSPITAL':
+				this.router.navigate([ManageRoutes.getFullRoute(apiCode), false, apiCode]);
+				break;
 			case 'PRC_REG':
+			case 'PRO-ASS':
+			case 'PRO-EXT':
+			case 'PRO-TRAN':
+			case 'PRO-DUP':
+			case 'PRO-NDU':
+			case 'PRO-VAC':
+			case 'PRO-ASSCER':
+			case 'PRO-REFUND':
+			case 'PRO-TAX-REBATE':
+			case 'WTR-NEW':
+			case 'WTR-DISCON':
+			case 'WTR-TRXF-OWN':
+			case 'WTR-CHNG-USG':
+			case 'WTR-RECON':
+			case 'WTR-PLUMB-LIC':
+			case 'WTR-RNW-PLUMB-LIC':
 				this.router.navigate([ManageRoutes.getFullRoute(apiCode)]);
 				break;
 			default:
-				this.createRecord();
+				if (ManageRoutes.getApiTypeFromApiCode(apiCode)) {
+					this.createRecord();
+				} else {
+					// todo
+					this.toastr.error("Invalid API Code");
+				}
 				break;
 		}
-		
+
 	}
 
 	/**
