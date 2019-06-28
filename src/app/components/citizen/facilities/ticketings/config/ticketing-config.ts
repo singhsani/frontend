c4daf59ccdb2e47d26bcc36e3242e40e1bcc3679
bhumika.barad <bhumika.barad@nascentinfo.com>
2019-06-28 14:47:38 +0530

    planetarium ticketing

--- src/app/components/citizen/facilities/ticketings/config/ticketing-config.ts
+++ src/app/components/citizen/facilities/ticketings/config/ticketing-config.ts
@@ -6,6 +6,13 @@ import { BTConfig, BTConstants } from '../../config/bt-config';
 export class TicketingConstants extends BTConstants {
   static AGREE_MESSAGE = 'Should be agree with given bank details';
   static TERMS_AND_CONDITION_MESSAGE = 'Should Accept the terms and condition of form';
+  // static TICKETING_FILE_UPLOAD_URL = 'api/attachment/ticketing/upload';
+  static TICKETING_FILE_UPLOAD_URL = 'api/attachment/booking/upload';
+  static SELECT_RESOURCE_MESSAGE = 'Should be select resource first';
+  static AVAILABLE_SEATS = 'Seats are available';
+  static NOT_AVAILABLE = 'Seats are not available';
+  static ALL_FEILD_REQUIRED_MESSAGE = "Select required feild"
+
 }
 
 /**
