a6ccffe3e3615791130bb01da4545e1fd9cb8250
Shantanu <shantanu.sangewar@nascentinfo.com>
2019-11-21 10:51:56 +0530

    Changes in env file for testing purpose

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: true,
 	envName: 'production',
 	envAPIServer: 'http://192.168.30.67:8080/',
-
+	// envAPIServer: '/VMC/',
 	//Usefull url and api url's.
 	adminUrl: 'http://' + window.location.hostname + ':' + window.location.port + '/vmcadminportal/',
 	citizenUrl: 'http://' + window.location.hostname + ':' + window.location.port + '/vmcportal/',
