c41869b3a1fce03773596068d7942b3bb7381a30
rajdeep <rajdeepsr07@gmail.com>
2020-10-02 13:15:36 +0530

    bank related issue resolve for Townhall and amphiTheater booking

---
+++ src/app/shared/components/bank-details-booking/bank-details-booking.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit,Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ValidationService } from '../../services/validation.service';
+import { BookingService } from 'src/app/components/citizen/facilities/bookings/shared-booking/services/booking-service.service';
+
+
+@Component({
+  selector: 'app-bank-details-booking',
+  templateUrl: './bank-details-booking.component.html',
+  styleUrls: ['./bank-details-booking.component.scss']
+})
+export class BankDetailsBookingComponent implements OnInit {
+
+  @Input('form') atithigruhForm: FormGroup;
+
+  stadiumTranslateKey: string = "citizenStadiumScreen";
+  BankOptions: any = [];
+
+  constructor(private bookingService: BookingService) { }
+
+
+  ngOnInit() {
+      
+    this.bookingLookups();
+    this.createFormControls();
+
+  }
+
+  createFormControls() {
+    this.atithigruhForm.addControl('bankName', new FormGroup({
+      code: new FormControl(null, [Validators.required]),
+    }));
+    this.atithigruhForm.addControl('accountHolderName', new FormControl(null, [Validators.required, Validators.maxLength(50), Validators.minLength(2)]));
+    this.atithigruhForm.addControl('accountNo', new FormControl(null, [Validators.required, Validators.maxLength(18), Validators.minLength(9)]))
+    this.atithigruhForm.addControl('ifscCode', new FormControl(null, [Validators.required, ValidationService.ifscCodeValidator]))
+  }
+
+  /**
+	* Method is used to get all bank lookups
+	*/
+  // bookingLookups() {
+  //   this.bookingService.getBankNames().subscribe(resp => {
+  //     this.BankOptions = resp.data;
+  //   });
+  // }
+
+  bookingLookups() {
+    this.bookingService.getDataFromLookups().subscribe(resp => {
+      this.BankOptions = resp.BANK;
+    })
+  
+  }
+
+}
