bfa0838614582651525dbe6a9711562e478810bf
jil <jilpatel4478@gmail.com>
2020-12-29 11:13:49 +0530

    remove validation of mobile numbe and email during transfer

--- src/app/components/citizen/licences/shop-and-establishmentAct/shop-lic-transfer/shop-lic-transfer.component.ts
+++ src/app/components/citizen/licences/shop-and-establishmentAct/shop-lic-transfer/shop-lic-transfer.component.ts
@@ -543,8 +543,8 @@ export class ShopLicTransferComponent implements OnInit {
 				code:[data.relationshipType ? (data.relationshipType.code ? data.relationshipType.code : null) :  null,[Validators.required]]
 			}),
 			
-			mobileNo: [data.mobileNo ? data.mobileNo : null, [Validators.required]],
-			emailId: [data.emailId ? data.emailId : null, [Validators.required, ValidationService.emailValidator]],
+			mobileNo: [data.mobileNo ? data.mobileNo : null],
+			emailId: [data.emailId ? data.emailId : null, [ValidationService.emailValidator]],
 		})
 
 	}
