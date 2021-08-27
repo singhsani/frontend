ffd06ff09e26f714381deb11bf4af16d5d73c1af
chirag.rathod <chirag.rathod@nascentinfo.com>
2021-08-27 11:08:08 +0530

    conflict resolved

--- src/app/components/citizen/facilities/bookings/modules/children-theater/book-children-theater/book-children-theater.component.ts
+++ src/app/components/citizen/facilities/bookings/modules/children-theater/book-children-theater/book-children-theater.component.ts
@@ -163,7 +163,7 @@ export class BookChildrenTheaterComponent implements OnInit {
         this.childrenTheaterApplicationForm = this._fb.group({
             //step 1
             organizationName: [null, [Validators.required, Validators.maxLength(25)]],
-            orgTelephoneNo: [null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+            orgTelephoneNo: [null, [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
             organizationAddress: this._fb.group(this.addressComp.addressControls()),
             programPurpose: [null, [Validators.required, Validators.maxLength(200)]],
 
