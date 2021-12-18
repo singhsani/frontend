5528997af3c99a6c6e3e04dad6babb47beab62fd
Vishal Kumar <vishal.kumar@nascentinfo.com>
2021-12-18 12:02:01 +0530

    #16957, Committed to fix water reconnection attachments validation issue

--- src/app/components/citizen/tax/water-supply/application-reconnection/Services/application-reconnection.service.ts
+++ src/app/components/citizen/tax/water-supply/application-reconnection/Services/application-reconnection.service.ts
@@ -12,49 +12,54 @@ export class ApplicationReconnectionService {
   searchByConnection(connectionNo: string) {
     return this.http.get(`${Constants.baseApiWaterUrl}connection/active/searchForReConnection?connectionNo=${connectionNo}`,
       { observe: 'response' })
-      .pipe(map((response: any) => response))
+      .pipe(map((response: any) => response));
   }
 
   getreconnectionDocUpload(reconnectionId: any) {
     return this.http.post(`${Constants.baseApiWaterUrl}reconnection/getDocumentList?reconnectionId=${reconnectionId}`,
       { observe: 'response' })
-      .pipe(map((response: any) => response))
+      .pipe(map((response: any) => response));
   }
 
   submitNewgen(reconnectionId: any) {
     return this.http.post(`${Constants.baseApiWaterUrl}reconnection/submitNewgen?reconnectionId=${reconnectionId}`,
       { observe: 'response' })
-      .pipe(map((response: any) => response))
+      .pipe(map((response: any) => response));
   }
 
   save(data: any) {
     return this.http.post(`${Constants.baseApiWaterUrl}reconnection/save`, data,
       { observe: 'response' })
-      .pipe(map((response: any) => response))
+      .pipe(map((response: any) => response));
   }
 
   
   getReconnectionDocuments(reconnectionId: any) {
     return this.http.post(`${Constants.baseApiWaterUrl}reconnection/getReconnectionDocuments?reconnectionId=${reconnectionId}`, null,
       { observe: 'response' })
-      .pipe(map((response: any) => response))
+      .pipe(map((response: any) => response));
   }
 
   approveOrDecline(data: any) {
     return this.http.post(`${Constants.baseApiWaterUrl}reconnection/newGenCallback`, data,
       { observe: 'response' })
-      .pipe(map((response: any) => response))
+      .pipe(map((response: any) => response));
   }
 
   finalApproveOrDecline(applicationNo: any) {
     return this.http.post(`${Constants.baseApiWaterUrl}reconnection/callback?applicationNo=${applicationNo}`, null,
       { observe: 'response' })
-      .pipe(map((response: any) => response))
+      .pipe(map((response: any) => response));
   }
 
   downloadFile(url: any) {
     return this.http.get(`${Constants.serverApiIp}${url}`,
       { responseType: "arraybuffer" })
-      .pipe(map((response: any) => response))
+      .pipe(map((response: any) => response));
+  }
+
+  getAttachmentList(serviceFormId) {
+    return this.http.get<Array<Object>>(
+      `${Constants.baseApiWaterUrl}reconnection/attachments?serviceFormId=${serviceFormId}`);
   }
 }
