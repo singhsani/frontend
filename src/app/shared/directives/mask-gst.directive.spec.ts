d7a61363beb5b0b113c7e687d73c1c0356bf28cb
Chetan Porwal <chetan.porwal@nascentinfo.com>
2022-06-28 11:01:52 +0530

    professional GSt validation

---
+++ src/app/shared/directives/mask-gst.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MaskGstDirective } from './mask-gst.directive';
+
+describe('MaskGstDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MaskGstDirective();
+    expect(directive).toBeTruthy();
+  });
+});
