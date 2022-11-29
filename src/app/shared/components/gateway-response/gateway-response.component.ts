e8dc15c4615d6f1aaa1126e0cedbe201f6635945
harsh Singh <harsh.singh@nasecentinfo.com>
2022-11-29 18:55:53 +0530

    print receipt in meat and fish

--- src/app/shared/components/gateway-response/gateway-response.component.ts
+++ src/app/shared/components/gateway-response/gateway-response.component.ts
@@ -264,22 +264,15 @@ export class GatewayResponseComponent implements OnInit {
 						this.formService.apiType = ManageRoutes.getApiTypeFromApiCode(payRespData.serviceDetail.code);
 						this.formService.submitFormData(payRespData.serviceFormId).subscribe(res => {
 							if (res) {
-								if (this.formService.apiType == 'APLicense' || this.formService.apiType == 'APLRenewal' || this.formService.apiType == 'APLTransfer') {
+								if (this.formService.apiType == 'APLicense' || this.formService.apiType == 'APLRenewal' || this.formService.apiType == 'APLTransfer' ||
+								this.formService.apiType == 'MFLicense' || this.formService.apiType == 'MFLRenewal' || this.formService.apiType == 'MFLTransfer') {
 									setTimeout(() => {
 										const url = '/citizen/my-applications' + '?id=' + payRespData.serviceFormId + '&apiCode=' + payRespData.serviceDetail.code
 										this.router.navigateByUrl(url);
 									}, 10000);
 
 									this.interVal();
-								}else if(this.formService.apiType == 'MFLicense' || this.formService.apiType == 'MFLRenewal' || this.formService.apiType == 'MFLTransfer'){
-									setTimeout(() => {
-										const url = '/citizen/my-applications' + '?id=' + payRespData.serviceFormId + '&apiCode=' + payRespData.serviceDetail.code
-										this.router.navigateByUrl(url);
-									}, 10000);
-
-									this.interVal();
-								} else {
-									setTimeout(() => {
+								}else {									setTimeout(() => {
 										this.redirectToMyApplication(data.myApplicationUrl, payRespData.refNumber, payData.resourceType, payRespData.payableServiceType);
 									}, 10000);
 
