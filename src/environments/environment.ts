d3f865de953a204b69cb13188718f405ef096ddf
chirag.rathod <chirag.rathod@nascentinfo.com>
2023-04-26 18:03:46 +0530

    Revert "Bug #23289, Conflict"
    This reverts commit 22b0a951f80d6124ef410a87fa9a80e77623a50c.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,15 +6,16 @@
 export const environment = {
 	production: false,
 	envName: 'dev',
-	//envAPIServer: '/VMC/',
+	envAPIServer: '/VMC/',
  	// envAPIServer: 'http://192.168.10.107:8080/VMC/',
 	// envAPIServer: 'http://192.168.10.202:8080/VMC/',
 	// envAPIServer: '/VMC/',
-	envAPIServer: window.location.protocol + "//" + window.location.hostname + ":8080/",
+	// envAPIServer: window.location.protocol + "//" + window.location.hostname + ":8080/",
 
 	//Usefull url and api url's.
-	adminUrl: window.location.protocol + "//" + window.location.hostname + ':' + window.location.port + '/',
-	citizenUrl: window.location.protocol + "//" + window.location.hostname + ':' + window.location.port + '/',
-	returnUrl: window.location.protocol + "//" + window.location.hostname + ':' + window.location.port + '/citizen/payment-response',
-	returnhosUrl: window.location.protocol + "//" + window.location.hostname + ':' + window.location.port + '/hospital/hos-payment-gateway-response'
+	adminUrl: window.location.protocol + "//" + window.location.hostname + ':' + window.location.port + '/vmcadminportal/',
+	citizenUrl: window.location.protocol + "//" + window.location.hostname + ':' + window.location.port + '/vmcportal/',
+	returnUrl: window.location.protocol + "//" + window.location.hostname + ':' + window.location.port + '/vmcportal/citizen/payment-response',
+	returnhosUrl: window.location.protocol + "//" + window.location.hostname + ':' + window.location.port + '/vmcportal/hospital/hos-payment-gateway-response'
 };
+
