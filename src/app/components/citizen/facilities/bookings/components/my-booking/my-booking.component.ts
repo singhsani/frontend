c39ffdf6f4d993d2e5e87fab672863a0728ed88b
aishwarya <aishwarya.soni@nascentinfo.com>
2023-06-19 17:00:56 +0530

    Townhall: Deposit receipt problem solved #23636

--- src/app/components/citizen/facilities/bookings/components/my-booking/my-booking.component.ts
+++ src/app/components/citizen/facilities/bookings/components/my-booking/my-booking.component.ts
@@ -884,7 +884,7 @@ export class MyBookingComponent implements OnInit {
 	}
 	showTownhallDepositReceiptBtn(element) {
 		if ((element.resourceType == 'TOWNHALL' && element.userType != 'Citizen')
-			&& (element.status == this.bookingConstant.BOOKED || element.status == this.bookingConstant.COMPLETED || element.status == this.bookingConstant.CANCELLATION_REQUEST || element.status == this.bookingConstant.REFUND_REQUEST || element.status == this.bookingConstant.CANCELLATION_APPROVED)) {
+			&& (element.status == this.bookingConstant.BOOKED || element.status == this.bookingConstant.COMPLETED || element.status == this.bookingConstant.CANCELLATION_REQUEST || element.status == this.bookingConstant.REFUND_REQUEST || element.status == this.bookingConstant.CANCELLATION_APPROVED) && (element.depositReceiptNo!=null)) {
 			return true;
 		} else {
 			return false;
